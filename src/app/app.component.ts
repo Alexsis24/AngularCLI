d173d9799f5c121a3a5dd6adc358b9fcf74f239b
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2022-06-10 14:26:49 +0300

    10.06.22 fixed push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCli_Project';
+  title = 'firstproject01';
 }
