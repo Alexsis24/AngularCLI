bd753d011361b783abe4f01640190f9cb37dff85
Alexsis24 <53233129+Alexsis24@users.noreply.github.com>
2022-06-14 14:06:37 +0300

    õpetaja põhi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, NgModule } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firstproject01';
+  title = 'AngularCrud1';
 }
